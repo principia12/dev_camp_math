6750584bfcf32b688040c9be6aa28e4f296eb3a9
name <principia_12@kaist.ac.kr>
2018-06-02 13:39:56 +0900

    hint for coding practice

--- src/skeleton/PyFormula.py
+++ src/skeleton/PyFormula.py
@@ -35,7 +35,79 @@ class PyFormula:
     def _calculate(tree, value_dict):
         pass
     
+'''
+6/02 Coding Practice 
+Parser Implementation
 
+In this problem, you are given a grammar that expresses common mathematical expressions, but without (-) signed numbers. 
+
+1. Grammar
+
+IMPORTANT : IF YOU CAN UNDERSTAND THE GRAMMAR, DO NOT READ THIS PART.
+
+Grammar is given as following; 
+
+1) expr -> part (binary part)* ; 
+2) part -> num | val | "(" expr ")" ; 
+3) binary -> "+" | "-" | "*" | "/" | "^" ; 
+4) num -> r"[1-9][0-9]*\.?[0-9]*|0" ; 
+5) val -> r"[a-zA-Z]+"
+
+Each line of grammar is called a rule. A rule can be seperated by '->', distinguishing left hand side and right hand side. Each rule means that the left hand side can be rewritten by one of the options - options are splited by | - on the left hand side. For example, binary can be replaced by "+". Input is always assumed to be the left hand side of the first rule, and parsed to the sequence of 'terminals', which are tokens that are wrapped by ". 
+
+Your task is to write a parser that gets input string and return a tree-like structure expressing the meaning of this grammar. 
+
+The grammar shown on the slide is NOT identical to the grammar you see in this text. However, it should not make a bit difference - unless you have passed all the tests till eq33. Even if so, it will not cause a significant error. 
+
+On rule 1), '(binary part)*' means that 'binary part' will be finitely iterated. 0 iteration is also possible. So, 1) is just a shortened notation for  
+
+1') expr -> part | part binary part | part binary part binary part | ... 
+
+On rule 4 and 5, you might find the right hand side of the rule unfamiliar if you have not encounted a regular expression before. They imply as follows; 
+
+4.r) r"[1-9][0-9]*\.?[0-9]*|0"
+
+Firstly, [1-9] or [0-9] means from 1-9 or 0-9, respectively. * after the [] term means that token that is expressed in [] can be repeated zero or more times. Likewise, ? means it can be repeated zero or one times. \. represents dot itself. So, this regular expression means a string that starts with one of the numbers 1 to 9, and have few numbers afterward, allowing one dot in the middle; which is just a normal numbers, except 0. Since 0 is the only number that starts with 0, 0 is handled with care. 
+
+5.r) r"[a-zA-Z]+"
+
+Only new part is +. It means one or more iteration, not allowing zero iteration. 
+ 
+
+2. Functions to implement and hints 
+
+ 
+- tokenizer
+
+When implementing tokenizer, go through a given equation by iteratively checking whether the first part of equation matches with one of the tokens. If so, erase matching part and repeat again. For this, use re.match() function. 
+
+re.match(pattern, string) checks whether the pattern is matched with the 'first part' of the string. (This is difference between re.find(); it searches through all the string.) Since it will return a match object, you can use m.start() and m.end() to locate the longest matching part. So the pseudocode will be as following; 
+
+while eq !=  '':
+    for pattern in tokens:
+        if re.match(pattern, eq):
+            yield (pattern_type, pattern)
+            eq = erase_eq(...) 
+
+Note that not all tokens are regular expression; some are just strings. Handle them accordingly. 
+
+- parser
+
+Two methods can be considered for implementing the parser for the given grammar. 
+
+- Recursive implementation 
+
+In the recursive implementation, 'expr' and 'part' will be a function that parses to that specific matching part and return the index of the matching part. For example, consider following expression; 
+
+1+1 -> tokenized to ('num', 1), ('op', +), ('num', 1)
+
+When you first call expr, it should call part twice; first part should return 1 or 2, since it will only parse the first token. You can either increment token, or make part function returns to the next index that parsing need to be started. 
+
+- Iterative implementation 
+
+Consider the type of current token and next token. Handle it accoridngly; use stack for handling the operators. 
+
+'''
 
 precedence = {\
     '+' : 1, 
@@ -62,21 +134,6 @@ def recursive_descent(tokens):
     
     return res
     
-def expr(operator, operand, tokens, idx):
-    return idx
-    
-def find_match(tokens, t_idx):
-    return t_idx 
-
-    
-def part(operator, operand, tokens, idx):
-    return idx
-    
-def pop_operator(operator, operand):
-    pass
-
-def push_operator(operator, operand, op):
-    pass
     
 def parse(eq):
     return recursive_descent(list(tokenizer(eq)))
